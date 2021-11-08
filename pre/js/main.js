5074ab7821ba05b3213c151459d1e92f61d873a4
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-11-08 11:30:00 +0100

    Trasposición de datos

--- pre/js/main.js
+++ pre/js/main.js
@@ -17,7 +17,7 @@ let dataSource = 'https://raw.githubusercontent.com/CarlosMunozDiazCSIC/envejeci
 let tooltip = d3.select('#tooltip');
 
 //Variables para visualización
-let innerData = [], chartBlock = d3.select('#chart'), chart, x_c, x_cAxis, y_c, y_cAxis;
+let innerData = [], chartBlock = d3.select('#chart'), chart, x_c, x_cAxis, y_c, y_cAxis, z_c;
 
 initChart();
 
@@ -27,17 +27,14 @@ function initChart() {
         if (error) throw error;
 
         innerData = csv.parse(data);
-        console.log(innerData);
 
         ///Agrupación de datos > Por edad y por tipo de conocimiento
         let edades = innerData.columns.slice(1);
         edades = edades.reverse();
 
-        let keys = innerData.forEach(function(item) {
-            return habilidades_digitales_2020;
+        let keys = innerData.map(function(item) {
+            return item.habilidades_digitales_2020;
         });
-
-        console.log(keys);
 
         //Desarrollo del gráfico > Debemos hacer muchas variables genéricas para luego actualizar el gráfico
         let margin = {top: 5, right: 17.5, bottom: 20, left: 50};
@@ -91,199 +88,32 @@ function initChart() {
         
         chart.append("g")
             .call(y_cAxis);
+
+        //Stacked data
+        z_c = d3.scaleOrdinal()
+            .range(['red', 'blue', 'yellow', 'green'])
+            .domain(keys);
+
+        //Visualización
+        chart.selectAll('.bar')
+            .data(d3.stack().keys(keys)(innerData))
+            .enter()
+            .append("g")
+            .attr("fill", function(d) { console.log(d); return z_c(d.key); })
+            .attr("class", function(d) { return 'g_rect rect-' + d.key; })
+            .selectAll("rect")
+            .data(function(d) { return d; })
+            .enter()
+            .append("rect")                
+            .attr("y", function(d) { console.log(d); return y_c(d.data.tipo) + y_c.bandwidth() / 4; })
+            .attr("x", function(d) { return x_c(0); })
+            .attr("height", y_c.bandwidth() / 2);
         
     });
 }
 
 function animateChart() {
-    //Animación de las tres líneas
-    path_1 = chart.select(".line-chart-1")
-        .data([data_65])
-        .attr("class", 'line-chart-1')
-        .attr("fill", "none")
-        .attr("stroke", color_1)
-        .attr("stroke-width", '2px')
-        .attr("d", line);
-
-    length_1 = path_1.node().getTotalLength();
-
-    path_1.attr("stroke-dasharray", length_1 + " " + length_1)
-        .attr("stroke-dashoffset", length_1)
-        .transition()
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        .duration(3000);
-
-    path_2 = chart.select(".line-chart-2")
-        .data([data_85])
-        .attr("class", 'line-chart-2')
-        .attr("fill", "none")
-        .attr("stroke", color_2)
-        .attr("stroke-width", '2px')
-        .attr("d", line);
-
-    length_2 = path_2.node().getTotalLength();
-
-    path_2.attr("stroke-dasharray", length_2 + " " + length_2)
-        .attr("stroke-dashoffset", length_2)
-        .transition()
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        .duration(3000);
-
-    path_3 = chart.select(".line-chart-3")
-        .data([data_100])
-        .attr("class", 'line-chart-3')
-        .attr("fill", "none")
-        .attr("stroke", color_3)
-        .attr("stroke-width", '2px')
-        .attr("d", line);
-
-    length_3 = path_3.node().getTotalLength();
-
-    path_3.attr("stroke-dasharray", length_3 + " " + length_3)
-        .attr("stroke-dashoffset", length_3)
-        .transition()
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        .duration(3000);
-
-    //Borramos todos los círculos de una tacada
-    chart
-        .selectAll('.circle-chart')
-        .remove();
-
-    chart.selectAll('circles')
-        .data(data_65)
-        .enter()
-        .append('circle')
-        .attr('class', 'circle-chart')  
-        .attr("r", '3.5')
-        .attr("cx", function(d) { return x_c(+d.Periodo)})
-        .attr("cy", function(d) { return y_c(+d.superviviente_total.replace(',','.')); })
-        .style('fill', 'transparent')
-        .style("stroke", color_1)
-        .style('opacity', '0')
-        .on('mouseenter mousedown mousemove mouseover', function(d, i, e) {
-            let circles = document.getElementsByClassName('circle-chart');
-
-            //Darle mayor presencia
-            for(let i = 0; i < circles.length;i++) {
-                circles[i].style.opacity = '0.4';
-            }
-            this.style.opacity = '1';
-
-            //Texto
-            let html = '<p class="chart__tooltip--title">' + d.Edad + ' ('+ d.Periodo +')</p>' + '<p class="chart__tooltip--text">Media de supervivientes: ' + numberWithCommas2(parseInt(d.superviviente_total.replace('.',','))) + '</p>' +
-            '<p class="chart__tooltip--text">Mujeres: ' + numberWithCommas2(parseInt(d.superviviente_mujer.replace('.',','))) + '</p>' + 
-            '<p class="chart__tooltip--text">Hombres: ' + numberWithCommas2(parseInt(d.superviviente_hombre.replace('.',','))) + '</p>';
-            
-            tooltip.html(html);
-
-            //Tooltip
-            positionTooltip(window.event, tooltip);
-            getInTooltip(tooltip);               
-        })
-        .on('mouseout', function(d, i, e) {
-            let circles = document.getElementsByClassName('circle-chart');
-            //Darle mayor presencia
-            for(let i = 0; i < circles.length;i++) {
-                circles[i].style.opacity = '1';
-            }
-            //Quitamos el tooltip
-            getOutTooltip(tooltip);                
-        })
-        .transition()
-        .delay(function(d,i) { return i * (3000 / data_65.length - 1)})
-        .style('opacity', '1');
-
-    chart.selectAll('circles')
-        .data(data_85)
-        .enter()
-        .append('circle')
-        .attr('class', 'circle-chart')  
-        .attr("r", '3.5')
-        .attr("cx", function(d) { return x_c(+d.Periodo)})
-        .attr("cy", function(d) { return y_c(+d.superviviente_total.replace(',','.')); })
-        .style('fill', 'transparent')
-        .style("stroke", color_2)
-        .style('opacity', '0')
-        .on('mouseenter mousedown mousemove mouseover', function(d, i, e) {
-            let circles = document.getElementsByClassName('circle-chart');
-
-            //Darle mayor presencia
-            for(let i = 0; i < circles.length;i++) {
-                circles[i].style.opacity = '0.4';
-            }
-            this.style.opacity = '1';
-
-            //Texto
-            let html = '<p class="chart__tooltip--title">' + d.Edad + ' ('+ d.Periodo +')</p>' + '<p class="chart__tooltip--text">Media de supervivientes: ' + numberWithCommas2(parseInt(d.superviviente_total.replace('.',','))) + '</p>' +
-            '<p class="chart__tooltip--text">Mujeres: ' + numberWithCommas2(parseInt(d.superviviente_mujer.replace('.',','))) + '</p>' + 
-            '<p class="chart__tooltip--text">Hombres: ' + numberWithCommas2(parseInt(d.superviviente_hombre.replace('.',','))) + '</p>';
-            
-            tooltip.html(html);
-
-            //Tooltip
-            positionTooltip(window.event, tooltip);
-            getInTooltip(tooltip);               
-        })
-        .on('mouseout', function(d, i, e) {
-            let circles = document.getElementsByClassName('circle-chart');
-            //Darle mayor presencia
-            for(let i = 0; i < circles.length;i++) {
-                circles[i].style.opacity = '1';
-            }
-            //Quitamos el tooltip
-            getOutTooltip(tooltip);                
-        })
-        .transition()
-        .delay(function(d,i) { return i * (3000 / data_85.length - 1)})
-        .style('opacity', '1');
     
-    chart.selectAll('circles')
-        .data(data_100)
-        .enter()
-        .append('circle')
-        .attr('class', 'circle-chart')  
-        .attr("r", '3.5')
-        .attr("cx", function(d) { return x_c(+d.Periodo)})
-        .attr("cy", function(d) { return y_c(+d.superviviente_total.replace(',','.')); })
-        .style('fill', 'transparent')
-        .style("stroke", color_3)
-        .style('opacity', '0')
-        .on('mouseenter mousedown mousemove mouseover', function(d, i, e) {
-            let circles = document.getElementsByClassName('circle-chart');
-
-            //Darle mayor presencia
-            for(let i = 0; i < circles.length;i++) {
-                circles[i].style.opacity = '0.4';
-            }
-            this.style.opacity = '1';
-
-            //Texto
-            let html = '<p class="chart__tooltip--title">' + d.Edad + ' ('+ d.Periodo +')</p>' + '<p class="chart__tooltip--text">Media de supervivientes: ' + numberWithCommas2(parseInt(d.superviviente_total.replace('.',','))) + '</p>' +
-            '<p class="chart__tooltip--text">Mujeres: ' + numberWithCommas2(parseInt(d.superviviente_mujer.replace('.',','))) + '</p>' + 
-            '<p class="chart__tooltip--text">Hombres: ' + numberWithCommas2(parseInt(d.superviviente_hombre.replace('.',','))) + '</p>';
-            
-            tooltip.html(html);
-
-            //Tooltip
-            positionTooltip(window.event, tooltip);
-            getInTooltip(tooltip);               
-        })
-        .on('mouseout', function(d, i, e) {
-            let circles = document.getElementsByClassName('circle-chart');
-            //Darle mayor presencia
-            for(let i = 0; i < circles.length;i++) {
-                circles[i].style.opacity = '1';
-            }
-            //Quitamos el tooltip
-            getOutTooltip(tooltip);                
-        })
-        .transition()
-        .delay(function(d,i) { return i * (3000 / data_100.length - 1)})
-        .style('opacity', '1');
 }
 
 document.getElementById('replay').addEventListener('click', function() {
